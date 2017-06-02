3cf673992dc15b8f1137667d39a42c0049416c56
Braden Farmer <farmerbb@gmail.com>
2017-06-01 21:29:41 -0600

    Don't show input method picker on lock screen if device is using file-based encryption

--- app/src/main/java/com/farmerbb/secondscreen/service/DisableKeyboardService.java
+++ app/src/main/java/com/farmerbb/secondscreen/service/DisableKeyboardService.java
@@ -19,6 +19,7 @@ import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -71,8 +72,11 @@ public class DisableKeyboardService extends InputMethodService {
             NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             nm.notify(notificationId, notification.build());
 
+            DevicePolicyManager devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
             KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
-            if(keyguardManager.inKeyguardRestrictedInputMode()) {
+
+            if(devicePolicyManager.getStorageEncryptionStatus() != DevicePolicyManager.ENCRYPTION_STATUS_ACTIVE_PER_USER
+                    && keyguardManager.inKeyguardRestrictedInputMode()) {
                 InputMethodManager manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
                 manager.showInputMethodPicker();
             }
