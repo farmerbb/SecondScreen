eb6304f1f293ac6f7c3c4506b639765b23ea5c63
Braden Farmer <farmerbb@gmail.com>
2019-10-08 21:13:29 -0600

    Remove unused import

--- app/src/main/java/com/farmerbb/secondscreen/fragment/dialog/FirstRunDialogFragment.java
+++ app/src/main/java/com/farmerbb/secondscreen/fragment/dialog/FirstRunDialogFragment.java
@@ -21,7 +21,6 @@ import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import com.farmerbb.secondscreen.R;
