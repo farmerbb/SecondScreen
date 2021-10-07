eb9f75a86cd944f39453fd0dbf5da55e01d79456
Braden Farmer <farmerbb@gmail.com>
2021-10-07 10:21:31 -0600

    Preliminary implementation of Shizuku

--- app/src/main/java/com/farmerbb/secondscreen/activity/UnableToStartActivity.java
+++ app/src/main/java/com/farmerbb/secondscreen/activity/UnableToStartActivity.java
@@ -16,10 +16,17 @@
 package com.farmerbb.secondscreen.activity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -28,13 +35,82 @@ import com.farmerbb.secondscreen.BuildConfig;
 import com.farmerbb.secondscreen.R;
 import com.farmerbb.secondscreen.util.U;
 
+import java.lang.reflect.Method;
+
+import rikka.shizuku.Shizuku;
+import rikka.shizuku.ShizukuBinderWrapper;
+import rikka.shizuku.ShizukuProvider;
+import rikka.shizuku.SystemServiceHelper;
+
 // This activity is responsible for informing the user that SecondScreen is unable to start.
-public final class UnableToStartActivity extends AppCompatActivity {
+public final class UnableToStartActivity extends AppCompatActivity implements Shizuku.OnRequestPermissionResultListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Shizuku.pingBinder()) {
+            boolean isGranted;
+            if(Shizuku.isPreV11() || Shizuku.getVersion() < 11) {
+                isGranted = checkSelfPermission(ShizukuProvider.PERMISSION) == PackageManager.PERMISSION_GRANTED;
+            } else {
+                isGranted = Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED;
+            }
+
+            if(isGranted) {
+                grantWriteSecureSettingsPermission();
+            } else {
+                int SHIZUKU_CODE = 123;
+                if(Shizuku.isPreV11() || Shizuku.getVersion() < 11) {
+                    requestPermissions(new String[] { ShizukuProvider.PERMISSION }, SHIZUKU_CODE);
+                } else {
+                    Shizuku.requestPermission(SHIZUKU_CODE);
+                }
+            }
+        } else {
+            proceedWithOnCreate();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int i = 0; i < permissions.length; i++) {
+            String permission = permissions[i];
+            int result = grantResults[i];
+
+            if(permission.equals(ShizukuProvider.PERMISSION)) {
+                onRequestPermissionResult(requestCode, result);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, int grantResult) {
+        boolean isGranted = grantResult == PackageManager.PERMISSION_GRANTED;
+        if(isGranted) {
+            grantWriteSecureSettingsPermission();
+        } else {
+            proceedWithOnCreate();
+        }
+    }
+
+    @SuppressLint("PrivateApi")
+    private void grantWriteSecureSettingsPermission() {
+        try {
+            Class<?> iPmClass = Class.forName("android.content.pm.IPackageManager");
+            Class<?> iPmStub = Class.forName("android.content.pm.IPackageManager$Stub");
+            Method asInterfaceMethod = iPmStub.getMethod("asInterface", IBinder.class);
+            Method grantRuntimePermissionMethod = iPmClass.getMethod("grantRuntimePermission", String.class, String.class, int.class);
+
+            Object iPmInstance = asInterfaceMethod.invoke(null, new ShizukuBinderWrapper(SystemServiceHelper.getSystemService("package")));
+            grantRuntimePermissionMethod.invoke(iPmInstance, BuildConfig.APPLICATION_ID, android.Manifest.permission.WRITE_SECURE_SETTINGS, 0);
+        } catch (Exception ignored) {}
+
+        proceedWithProfileLoad();
+    }
+
+    private void proceedWithOnCreate() {
         setContentView(R.layout.activity_turn_off);
         setTitle(R.string.permission_needed);
         setFinishOnTouchOutside(false);
@@ -50,26 +126,28 @@ public final class UnableToStartActivity extends AppCompatActivity {
         Button button2 = findViewById(R.id.turnOffButtonSecondary);
 
         button1.setText(R.string.action_continue);
-        button1.setOnClickListener(v -> {
-            if(getIntent().hasExtra("action")) {
-                if(U.hasElevatedPermissions(this, true)) {
-                    switch(getIntent().getStringExtra("action")) {
-                        case "load-profile":
-                            U.loadProfile(this, getIntent().getStringExtra("filename"));
-                            break;
-                        case "turn-off-profile":
-                            U.turnOffProfile(this);
-                            break;
-                    }
-                }
-            } else
-                LocalBroadcastManager.getInstance(this)
-                        .sendBroadcast(new Intent("com.farmerbb.secondscreen.SHOW_DIALOGS"));
-
-            finish();
-        });
+        button1.setOnClickListener(v -> proceedWithProfileLoad());
 
         button2.setVisibility(View.GONE);
+    }
+
+    private void proceedWithProfileLoad() {
+        if(getIntent().hasExtra("action")) {
+            if(U.hasElevatedPermissions(this, true)) {
+                switch(getIntent().getStringExtra("action")) {
+                    case "load-profile":
+                        U.loadProfile(this, getIntent().getStringExtra("filename"));
+                        break;
+                    case "turn-off-profile":
+                        U.turnOffProfile(this);
+                        break;
+                }
+            }
+        } else
+            LocalBroadcastManager.getInstance(this)
+                    .sendBroadcast(new Intent("com.farmerbb.secondscreen.SHOW_DIALOGS"));
+
+        finish();
     }
 
     // Disable the back button
